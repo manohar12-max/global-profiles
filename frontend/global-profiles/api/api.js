be91ef4f65f1936ad38635a42b757a6956b395dc
Manohar Dhumal <2898manohar@gmail.com>
2025-05-24 02:00:10 +0530

    2nd commit

--- frontend/global-profiles/api/api.js
+++ frontend/global-profiles/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 //"https://blog-6bpi.onrender.com/api"
 //http://localhost:5000/api
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://global-profiles.onrender.com/api',
 });
 
 export default api;
